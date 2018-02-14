d668605e17d4d13d2367d1a32c811e20cd447e93
DoktorHolmes <victoryuska@gmail.com>
2018-02-14 01:26:21 -0500

    Colored biomes, map generation options, & more
    Added colored biomes on the map. (Script now requires ansicolors module)
    Added new map generation options such as temperature offset, sea level, number of octaves, and a numerical seed

--- worldsim.py
+++ worldsim.py
@@ -4,20 +4,27 @@
 import random
 import sys
 import os
+import time
 import noise
 from noise import pnoise2, snoise2, snoise3
+from colors import red, green, yellow, blue, magenta, cyan, white, color
 
 from faker import Faker #Temporary name generation module
 
 class World:
-	def __init__(self, width, height, lushness, richness):
+	def __init__(self, width, height, lushness, richness, sealevel, temperature, seed):
+		if(seed != None):
+			self.seed = seed
+		else:
+			self.seed = random.randrange(1, 99999)
 		self.height = height
 		self.width = width
 		self.lushness = lushness
 		self.richness = richness
+		self.temperature = temperature
 		self.tileTypes = ["~", "#", "^", "J", "C"]
 		self.tiles = []
-		self.sealevel = 115
+		self.sealevel = sealevel
 		self.races = [Race(0, "Skerran", "Avian", 1000, "W", 45), Race(0, "Ravakan", "Reptilian", 850, "P", 60), Race(0, "Ahn'litend", "Artificial", 350, "F", 245)]
 	def generateMap(self):
 		for x in range(0, self.width):
@@ -29,7 +36,24 @@ class World:
 		for y in range(0, self.height):
 			print("")
 			for x in range(0, self.width):
-				sys.stdout.write(self.tiles[x][y].type)
+				if(self.tiles[x][y].color == "cyan"):
+					sys.stdout.write(cyan(self.tiles[x][y].type))
+				elif(self.tiles[x][y].color == "white"):
+					sys.stdout.write(white(self.tiles[x][y].type))
+				elif(self.tiles[x][y].color == "green"):
+					sys.stdout.write(green(self.tiles[x][y].type))
+				elif(self.tiles[x][y].color == "yellow"):
+					sys.stdout.write(yellow(self.tiles[x][y].type))
+				elif(self.tiles[x][y].color == "blue"):
+					sys.stdout.write(blue(self.tiles[x][y].type))
+				elif(self.tiles[x][y].color == "magenta"):
+					sys.stdout.write(magenta(self.tiles[x][y].type))
+				elif(self.tiles[x][y].color == "red"):
+					sys.stdout.write(red(self.tiles[x][y].type))
+				elif(self.tiles[x][y].colorX != None):
+					sys.stdout.write(color(self.tiles[x][y].type, self.tiles[x][y].colorX))
+				else:
+					sys.stdout.write(self.tiles[x][y].type)
 	def printBiomeMap(self):
 		for y in range(0, self.height):
 			print("")
@@ -38,45 +62,51 @@ class World:
 				
 	
 	def generatePerlinMap(self, octaves):
-		freq = 11.5 * octaves
-		seed = random.randrange(1, 1000)
+		freq = 16.0 * octaves
 		for x in range(self.width):
 			for y in range(self.height):	
-				elevation = int(snoise3(x / freq, y / freq, seed, octaves) * 127.0 + 128.0)
+				elevation = int(snoise3(x / freq, y / freq, self.seed, octaves) * 127.0 + 128.0)
 				self.tiles[x][y].elevation = elevation
-				if(elevation > 200):
-					self.tiles[x][y].type = "A"
-				elif(elevation > 140):
-					self.tiles[x][y].type = "#"
+				if(elevation >= self.sealevel + 85):
+					self.tiles[x][y].type = "^"
 				elif(elevation >= self.sealevel):
-					self.tiles[x][y].type = "="
+					self.tiles[x][y].type = "#"
 				else:
 					self.tiles[x][y].type = "~"
 	def generateBiomeMap(self, octaves):
-		freq = 15.0 * octaves
-		seed = random.randrange(1, 1000)
+		freq = 13.0 * octaves
 		for x in range(self.width):
 			for y in range(self.height):	
-				biome = int(snoise3(x / freq, y / freq, seed, octaves) * 127.0 + 128.0)
+				biome = int(snoise3(x / freq, y / freq, self.seed, octaves) * 127.0 + 128.0)
+				biome += self.temperature
 				if(self.tiles[x][y].elevation >= self.sealevel):
-					if(biome > 170):
-						self.tiles[x][y].biome = "A" #Arctic
-					elif(biome > 160):
-						self.tiles[x][y].biome = "T" #Tundra
-					elif(biome > 135):
-						self.tiles[x][y].biome = "F" #Forest
-					elif(biome > 105):
-						self.tiles[x][y].biome = "P" #Plains
-					elif(biome > 85):
-						self.tiles[x][y].biome = "W" #Wetlands
-					elif(biome > 65):
-						self.tiles[x][y].biome = "J" #Jungle
-					elif(biome > 35):
-						self.tiles[x][y].biome = "D" #Desert
+					if(self.tiles[x][y].elevation >= self.sealevel + 85):
+						self.tiles[x][y].biome = white("^")
+						self.tiles[x][y].color = "white"
+					elif(biome < 30):
+						self.tiles[x][y].biome = cyan("A") #Arctic
+						self.tiles[x][y].color = "cyan"
+					elif(biome < 80):
+						self.tiles[x][y].biome = white("T") #Tundra
+						self.tiles[x][y].color = "white"
+					elif(biome < 145):
+						self.tiles[x][y].biome = green("F") #Forest
+						self.tiles[x][y].color = "green"
+					elif(biome < 165):
+						self.tiles[x][y].biome = color("P", 154) #Plains
+						self.tiles[x][y].colorX = 154
+					elif(biome < 185):
+						self.tiles[x][y].biome = color("W", 94) #Wetlands
+						self.tiles[x][y].colorX = 94
+					elif(biome < 225):
+						self.tiles[x][y].biome = color("J", 64) #Jungle
+						self.tiles[x][y].colorX = 64
 					else:
-						self.tiles[x][y].biome = "B" #Barren
+						self.tiles[x][y].biome = color("D", 215) #Desert
+						self.tiles[x][y].colorX = 215
 				else:
-					self.tiles[x][y].biome = "~" #Ocean
+					self.tiles[x][y].biome = blue("~") #Ocean
+					self.tiles[x][y].color = "blue"
 				
 				
 class Tile:
@@ -98,18 +128,20 @@ class Race:
 		self.lifeSpan = lifeSpan
 
 global world
-world = World(100, 30, 30, 20)
+world = World(117, 50, 30, 20, 120, 0, None)
 world.generateMap()
-world.generatePerlinMap(1)
+world.generatePerlinMap(2)
 world.generateBiomeMap(1)
 
 def clearScreen():
 	os.system("cls")
 
 	
-def game(world):
+def game():
+	global world
 	while True:
-		world.printMap()
+		clearScreen()
+		world.printBiomeMap()
 		print("\n\nWelcome to WorldSim! A simulation of a planet and its inhabitants")
 		print("Enter a command below to navigate")
 		print("COMMANDS: races map sim help")
@@ -126,26 +158,57 @@ def game(world):
 		elif(inp == "map"):
 			while True:
 				clearScreen()
-				world.printMap()
-				print("\n\nCOMMANDS: biomes height back")
+				world.printBiomeMap()
+				print("\n\nCOMMANDS: biomes height generate seed back")
 				inp2 = input(">: ")
 				if(inp2 == "biomes"):
 					clearScreen()
 					world.printBiomeMap()
-					print("\nKEY: \nA - Arctic\nT - Tundra\nF - Forest\nP - Plains\nW - Wetlands\nJ - Jungle\nD - Desert\nB - Barren")
+					print("\nKEY: \nA - Arctic\nT - Tundra\nF - Forest\nP - Plains\nW - Wetlands\nJ - Jungle\nD - Desert\n^ - Mountain")
 					input("\nPRESS ENTER TO CONTINUE")
 				elif(inp2 == "height"):
 					clearScreen()
 					world.printMap()
 					input("\nPRESS ENTER TO CONTINUE")
+				elif(inp2 == "seed"):
+					clearScreen()
+					print("Current map seed is " + str(world.seed))
+					input("\nPRESS ENTER TO CONTINUE")
+				elif(inp2 == "generate"):
+					while True:
+						clearScreen()
+						try:
+							width = int(input("Enter the width of the world: "))
+							height = int(input("Enter the height of the world: "))
+							sealevel = int(input("Enter the sea level (Suggested: 122 to 140):"))
+							seed = input("Enter a seed if you want one, otherwise press enter for a random seed:")
+							if(seed != ""):
+								seed = int(seed)
+							else:
+								seed = None
+							temperature = int(input("Enter temperature offset (Suggested: -20 to 20): "))
+							octaves = int(input("Enter a number of octaves (Suggested: 1 to 4): "))
+							print("Generating...")
+							world = World(width, height, 30, 20, sealevel, temperature, seed)
+							world.generateMap()
+							world.generatePerlinMap(octaves)
+							world.generateBiomeMap(1)
+							print("Generation success!")
+							time.sleep(1.0)
+							break
+						except ValueError:
+							print("Please enter valid numbers for the width, height, sea level, seed, and octaves!")
+							
+					
 				elif(inp2 == "back"):
 					clearScreen()
+					world.printBiomeMap()
 					break
 				else:
 					print("Invalid command!")
-					print("COMMANDS: biomes height back")
 		else:
 			print("Invalid command!")
 			print("COMMANDS: races map sim help")
 			
-game(world)
+			
+game()
